988c85cf83b8109ef428dd799628a43f93314377
ilg2005 <igoldfarb10@gmail.com>
2022-07-12 21:23:36 +0300

    layout - yield | extends - section

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,19 +1,10 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Blog</title>
-    <link rel="stylesheet" href="./app.css">
-</head>
-<body>
-@foreach ($posts as $post)
-    <article>
-        <a href="/posts/{{$post->slug}}"><h1>{{$post->title}}</h1></a>
-        <p>{{$post->excerpt}}</p>
-    </article>
-@endforeach
-</body>
-</html>
+@extends('layout')
+
+@section('content')
+    @foreach ($posts as $post)
+        <article>
+            <a href="/posts/{{$post->slug}}"><h1>{{$post->title}}</h1></a>
+            <p>{{$post->excerpt}}</p>
+        </article>
+    @endforeach
+@endsection
