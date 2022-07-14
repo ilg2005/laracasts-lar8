8a3a2a3a96417e8fd6353f891d9b69bb375221bd
ilg2005 <igoldfarb10@gmail.com>
2022-07-14 14:17:25 +0300

    Post Model

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <article>
         <h1> {{$post->title}} </h1>
-        {!! $post->body !!}
+        <p>{{ $post->body }}</p>
     </article>
     <a href="/">Go Back</a>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,6 @@ Route::get('/', function () {
 
 Route::get('posts/{post}', function ($slug) {
     return view('post', [
-        'post' => Post::find($slug)
+        'post' => Post::where('slug', $slug)->first()
     ]);
 })->where('post', '[A-z_\-]+');
