1ce2f7cc2f180134e027c5f974748deb3dca4da9
ilg2005 <igoldfarb10@gmail.com>
2022-07-16 09:59:26 +0300

    show all posts associated with a category

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,7 +1,10 @@
 <x-layout>
     <article>
         <h1> {{$post->title}} </h1>
-        <p><a href="#">{{ $post->category->name }}</a></p>
+        <p>
+            <a href="/categories/{{ $post->category->slug }}">
+                {{ $post->category->name }}</a>
+        </p>
         <p>{{ $post->body }}</p>
     </article>
     <a href="/">Go Back</a>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,7 +2,7 @@
     @foreach ($posts as $post)
         <article>
             <a href="/posts/{{$post->slug}}"><h1>{{$post->title}}</h1></a>
-            <p><a href="#">{{ $post->category->name }}</a></p>
+            <p><a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
             <p>{{$post->excerpt}}</p>
         </article>
     @endforeach
